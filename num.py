9cf76380ef4da6a72f0dbca2a0c634241657c78a
NneamakaOluonye <ammaoluonye003@gmail.com>
2023-11-14 14:57:16 +0100

    first commit

---
+++ num.py
@@ -0,0 +1,32 @@
+# NUMBERS
+# x = 1
+# y = 2.5
+# z = -2.69j
+# print(type(x),type(y), type(z))
+# print(float(x), complex(y))
+
+# age = '23'
+# year = 2023
+# gpa = '2.9'
+# print(int(age), str(year), float(gpa) )
+
+# VARIABLES
+# name = 'John'
+
+# def myFunction():
+#     age = 29
+
+# print (name)
+
+# a = '23a'
+# b = 9.27
+# c = 27
+# d = '27'
+
+# print(int(a[0:2]))
+# print(str(b))
+# print(c == int(d))
+
+
+name = input('What is your name?')
+print ('My name is {name}'.format(name=name))
